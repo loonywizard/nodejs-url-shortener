96361b937981463a832175947933110873181789
Vladimir Nikitin <vladimir.nikitin@kupibilet.ru>
2021-03-21 21:41:06 +0300

    add api route for redirecting from short url to full url

--- server/index.ts
+++ server/index.ts
@@ -3,7 +3,9 @@ import webpack from 'webpack'
 import { Server as HTTPServer } from 'http'
 import webpackDevMiddleware from 'webpack-dev-middleware'
 import webpackConfig from '../webpack.config'
+import { IURL, URLModel } from './models/url'
 import { connectToDB } from './db'
+
 
 const PORT = 3000
 const app = express()
@@ -35,6 +37,22 @@ app.get('/', (req: Request, res: Response) => {
 
 app.get('/api/data', (req: Request, res: Response) => {
   res.send('hello from nodejs server!')
+})
+
+app.get('*', async (req: Request, res: Response) => {
+  try {
+    // TODO: req.path.slice(1) refactor
+    const url: IURL | null = await URLModel.findOne({ shortUrl: req.path.slice(1) }).exec()
+
+    // TODO: better 404 landing
+    if (url === null) return res.status(400).send('400')
+    
+    res.redirect(url.fullUrl)
+  } catch (error) {
+    console.error(error)
+
+    res.status(500).send('Internal Server Error')
+  }
 })
 
 async function startHttpServer() {
