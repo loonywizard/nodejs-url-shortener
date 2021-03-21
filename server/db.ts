09dda72add5a27949a18ef9c34c2d48d3f089971
Vladimir Nikitin <vladimir.nikitin@kupibilet.ru>
2021-03-21 20:59:04 +0300

    create mongoose url model and init test data

--- server/db.ts
+++ server/db.ts
@@ -1,5 +1,32 @@
-import mongoose from 'mongoose'
+import mongoose, { model, Schema, Document } from 'mongoose'
 
+
+interface IURL extends Document {
+  shortUrl: string,
+  fullUrl: string,
+}
+
+const URLSchema = new Schema<IURL>({
+  shortUrl: { type: String, required: true },
+  fullUrl: { type: String, required: true },
+}) 
+
+const URLModel = model<IURL>('URL', URLSchema)
+
+async function initTestData() {
+  const url1: IURL = await URLModel.create({ shortUrl: 'goo', fullUrl: 'https://google.com' })
+  const url2: IURL = await URLModel.create({ shortUrl: 'yt', fullUrl: 'https://youtube.com' })
+
+  await Promise.all(
+    [url1, url2].map((cat) => cat.save())
+  ).then(() => console.log('urls saved to DB!'))
+
+  URLModel.find((err, urls) => {
+    if (err) return console.error(err)
+    
+    console.log(urls)
+  })
+}
 
 async function connectToDB(): Promise<void> {
   try {
@@ -7,6 +34,8 @@ async function connectToDB(): Promise<void> {
       useNewUrlParser: true,
       useUnifiedTopology: true
     })
+
+    await initTestData()
   } catch (error) {
     console.error(`connection error: ${error}`)
 
