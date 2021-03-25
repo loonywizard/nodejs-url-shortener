6fa056f709e59b7a217cefbe9439b9ee0ba7421b
Vladimir Nikitin <vladimir.nikitin@kupibilet.ru>
2021-03-25 11:59:11 +0300

    pass env variables to mongoose.connect

--- server/db.ts
+++ server/db.ts
@@ -2,7 +2,9 @@ import mongoose from 'mongoose'
 
 async function connectToDB(): Promise<void> {
   try {
-    await mongoose.connect('mongodb://localhost:27017/my-db', {
+    const { DB_URL, DB_NAME, DB_USER, DB_PASSWORD } = process.env
+
+    await mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_URL}/${DB_NAME}`, {
       useNewUrlParser: true,
       useUnifiedTopology: true
     })
